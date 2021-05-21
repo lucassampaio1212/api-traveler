e332bf1eadca8b7658310850e5f520f9f0c255c7
lucas <jfontinele9490@gmail.com>
2021-05-21 16:06:30 -0300

    Projeto final finalizado

---
+++ src/shared/container/providers/StorageProviders/index.ts
@@ -0,0 +1,16 @@
+import { container } from "tsyringe";
+import LocalStorageProvider from "./implementations/LocalStorageProvider";
+import S3storageProvider from "./implementations/S3StorageProvider";
+import IStorageProvider from "./IStorageProviders";
+
+
+
+const providers = {
+  disk: LocalStorageProvider,
+  s3: S3storageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  providers[process.env.STORAGE_PROVIDER]
+);
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,27 +1,8 @@
-import { container } from "tsyringe";
-import IDateProvider from "./DateProvider/IDateProvider";
-import DayjsDateProvider from "./DateProvider/implementations/DayjsProvider";
-import IMailProvider from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import LocalStorageProvider from "./StorageProviders/implementations/LocalStorageProvider";
-import S3storageProvider from "./StorageProviders/implementations/S3StorageProvider";
-import IStorageProvider from "./StorageProviders/IStorageProviders";
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProviders";
 
 
 
-container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
 
-container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
-);
 
-const providers = {
-  disk: LocalStorageProvider,
-  s3: S3storageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  providers[process.env.STORAGE_PROVIDER]
-);
